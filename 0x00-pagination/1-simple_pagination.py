3deeb00632e37707870bfc4ce11aa8ad3573d0f8
Samuel Sylvester <samsylvester09@gmail.com>
2024-08-19 04:06:22 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,3 +2,40 @@
 """
 Copies index_range from the previous task
 and a class into the code"""
+
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculate the start and end index for the given page and page size.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
