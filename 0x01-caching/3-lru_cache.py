f203ba1fd6e1387c852483d1776ace09cdbce6c1
Samuel Sylvester <samsylvester09@gmail.com>
2024-08-22 00:48:06 +0100

    Updating the put and get in LRU file

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -21,20 +21,28 @@ class LRUCache(BaseCaching):
         """ Add an item in the cache following LRU """
         if key is None or item is None:
             return
-        # check if cache is full
+
+        # If the key is already in cache, update it and move it to the end of the list
+        if key in self.caches:
+            self.caches.remove(key)
+
+        # Check if cache is full
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lru_key = self.caches.pop(0)  # discard least item (LRU)
+            lru_key = self.caches.pop(0)  # Discard the least recently used item (LRU)
             del self.cache_data[lru_key]
             print(f"DISCARD: {lru_key}")
+
+        # Add the new key to the cache and update its position
         self.caches.append(key)
-        self.cache_data[key] = item  # assign item to key in cache_data
+        self.cache_data[key] = item  # Assign item to key in cache_data
 
     def get(self, key):
-        """
-        Get an item by key and
-        Return value in self.cache_data linked to key
-        If key is None or key does not exist in self.cache_data
-        """
+        """ Get an item by key following LRU """
         if key is None or key not in self.cache_data:
             return None
+
+        # Move the accessed key to the end of the list (most recently used)
+        self.caches.remove(key)
+        self.caches.append(key)
+        
         return self.cache_data.get(key)
