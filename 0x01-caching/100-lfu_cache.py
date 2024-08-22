25ac10b887beb21f53a43d0ee425503b6bb6f981
Samuel Sylvester <samsylvester09@gmail.com>
2024-08-22 01:49:49 +0100

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -14,19 +14,22 @@ class LFUCache(BaseCaching):
         """ Initialize LFU Cache """
         super().__init__()
         self.freqs = {}  # To store frequencies of items
-        self.timestamps = []  # To track insertion order for LRU when frequencies are tied
+        self.order = []  # To track insertion order for LRU
 
     def put(self, key, item):
         """ Add an item in the cache following LFU """
         if key is None or item is None:
             return
 
-        # If the key is already in the cache, update its value and frequency
+        """If the key is already in the cache,
+        update its value and frequency
+        """
         if key in self.cache_data:
             self.cache_data[key] = item
             self.freqs[key] += 1
-            self.timestamps.remove(key)  # Update position in timestamps for LRU
-            self.timestamps.append(key)  # Reinsert to end to mark it as recently used
+            self.order.remove(key)  # Update position in order
+            # Reinsert to end to mark it as recently used
+            self.order.append(key)
         else:
             # Check if cache is full
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
@@ -35,25 +38,29 @@ class LFUCache(BaseCaching):
                 # Collect all keys with the minimum frequency
                 lfu_keys = [k for k, v in self.freqs.items() if v == min_freq]
 
-                # If multiple LFU keys exist, use LRU to remove the oldest
+                """If multiple LFU keys exist,
+                use LRU to remove the oldest
+                """
                 if len(lfu_keys) > 1:
-                    for k in self.timestamps:
+                    for k in self.order:
                         if k in lfu_keys:
                             lfu_key = k
                             break
                 else:
                     lfu_key = lfu_keys[0]
 
-                # Remove the least frequently used (or least recently used) key
+                """Remove the least frequently used
+                (or least recently used) key
+                """
                 del self.cache_data[lfu_key]
                 del self.freqs[lfu_key]
-                self.timestamps.remove(lfu_key)
+                self.order.remove(lfu_key)
                 print(f"DISCARD: {lfu_key}")
 
             # Add the new key to the cache and initialize its frequency
             self.cache_data[key] = item
             self.freqs[key] = 1
-            self.timestamps.append(key)  # Track insertion order
+            self.order.append(key)  # Track insertion order
 
     def get(self, key):
         """ Get an item by key following LFU """
@@ -62,7 +69,7 @@ class LFUCache(BaseCaching):
 
         # Update the frequency and timestamps
         self.freqs[key] += 1
-        self.timestamps.remove(key)  # Update position in timestamps for LRU
-        self.timestamps.append(key)  # Reinsert to end to mark it as recently used
+        self.order.remove(key)  # Update position in timestamps for LRU
+        self.order.append(key)  # Reinsert to end to mark it as recently used
 
         return self.cache_data.get(key)
