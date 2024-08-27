07e4a283680512ff49c08eb7f4b27d1de809677d
Samuel Sylvester <samsylvester09@gmail.com>
2024-08-27 22:33:25 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
 """ A Basic Flask App"""
 
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
