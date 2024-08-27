fa290e82d0a7285c23e57e047b3bcdc0071d2811
Samuel Sylvester <samsylvester09@gmail.com>
2024-08-27 22:38:05 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Module that instantiate
+the Babel object in your app"""
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -11,14 +14,16 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
 babel = Babel(app)
 
 
 @app.route('/')
 def index():
+    """Render the index template."""
     return render_template('1-index.html')
 
 
-if __name__ == "__main__":
+while __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
