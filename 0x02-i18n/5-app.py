f9bbfe7c699175e97b10b58c7d2bea823cbd8915
Samuel Sylvester <samsylvester09@gmail.com>
2024-08-30 23:57:16 +0100

    Update 5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""flask module that access user data"""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
@@ -12,7 +13,8 @@ users = {
 
 
 class Config:
-    """App configuration."""
+    """App configuration for the flask
+    module"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
