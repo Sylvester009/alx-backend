33b69965e3b0bbe3a0bbd626c07fec295ac44be9
Samuel Sylvester <samsylvester09@gmail.com>
2024-09-26 05:38:49 +0100

    job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,28 @@
+#!/usr/bin/yarn dev
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const data = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account'
+};
+
+const job = queue.create('push_notification_code', data);
+
+job
+  .on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed', () => {
+    console.log('Notification job failed');
+  })
+  
+  .save((error) => {
+    if (error) {
+      console.log('Error creating job:', error);
+    }
+  });
